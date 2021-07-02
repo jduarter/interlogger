081ce221eb0166f6e772a3d192bbe129be190ead
Jorge Duarte <info@malagadev.com>
2021-07-02 08:08:03 +0200

    build: fixes issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
 import analyze from 'rollup-plugin-analyzer';
 import license from 'rollup-plugin-license';
 
-const path=require('path');
+const path = require('path');
 
 const LICENSE_CFG = {
   banner: {
@@ -21,13 +20,16 @@ export default [
     input: 'src/index.ts',
     output: [
       { file: 'build/interlogger.js', format: 'cjs' },
-      { file: 'build/interlogger.min.js', format: 'cjs', plugins: [terser()] },
+      {
+        file: 'build/interlogger.min.js',
+        format: 'cjs',
+        plugins: [terser()],
+      },
       { file: 'build/interlogger.esm.js', format: 'esm' },
     ],
     plugins: [
       typescript(),
       cleanup({ comments: 'none' }),
-      nodeResolve(),
       license(LICENSE_CFG),
       analyze(),
     ],
